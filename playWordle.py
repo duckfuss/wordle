b5cc9764f1a144c5dcd84505206b6bf236acd339
duckfuss <dtfussey@icloud.com>
2025-06-20 15:43:39 +0100

    Refactor playWordle.py to improve data reading and add likeness and fancyPrint functions for enhanced game logic

--- playWordle.py
+++ playWordle.py
@@ -1,7 +1,31 @@
-linkList = open("data/linkFile.txt").read()
-scoreList = list(open("data/scoreFile.txt").read())
+linkList = open("data/linkFile.txt").read().splitlines()
+scoreList = open("data/scoreFile.txt").read().splitlines()
 answers = open("data/answers.txt").read().splitlines()
 
 
+def likeness(word1, word2):
+    green, yellow = [], []
+    for i in range(5):
+        if word1[i] == word2[i]:
+            green.append((word1[i], i))
+        elif word1[i] in word2:
+            yellow.append((word1[i], i))
+    for letter, index in yellow: # discard duplicate yellows
+        word1Tot = yellow.count(letter) + green.count(letter)
+        word2Tot = word2.count(letter)
+        diff = word1Tot - word2Tot
+        if diff > 0:
+            for i in range(diff):
+                yellow.remove(letter)
+    return [green, yellow]
 
-print(linkList[0])
+def fancyPrint(green, yellow):
+    printy = ['⬛️','⬛️','⬛️','⬛️','⬛️']
+    for (char, index) in green:
+        printy[index] = "🟩"
+    for (char, index) in yellow:
+        printy[index] = "🟨"
+    print("".join(printy))
+
+green, yellow = likeness("slate", "hello")
+fancyPrint(green, yellow)
